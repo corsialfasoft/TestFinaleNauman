25f8a79539ed53c213dd48a5c0128fc5b117760a
naumanaziz <aziznauman96@hotmail.it>
2018-05-09 12:05:05 +0200

    primo esercizio

--- TestFinale/Program.cs
+++ TestFinale/Program.cs
@@ -5,8 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TestFinale {
-    class Program {
-        static void Main(string[] args) {
+    class Esercizio1 {
+        static void Main() {
+            int i = 0;
+            for (; i<=15; i++) {
+                if(i%2 == 0) {
+                    Console.WriteLine(i);    
+                }
+            }
+            Console.WriteLine("-----");
+            for (; 1 <= i; i--) {
+                if (i % 2 == 1) {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
