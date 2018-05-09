bce28b85893f79cb4343a3e01ff59ecee6b8871f
naumanaziz <aziznauman96@hotmail.it>
2018-05-09 15:37:50 +0200

    manca la procedura di AddMenu e il dao

---
+++ DDL.sql
@@ -0,0 +1,53 @@
+﻿create database GestioneMenu;
+use GestioneMenu;
+create table Portate(
+	id int identity(1,1) primary key, 
+	descrizione nvarchar(50)	
+);
+create table Piatti(
+	id int identity(1,1) primary key,
+	portata int foreign key references Portate, 
+	descrizione nvarchar(50)	
+);
+create table FasciaOrario(
+	id int identity(1,1) primary key,
+	descrizione nvarchar(50)	
+);
+create table Giorni(
+	id int identity(1,1) primary key, 
+	dataGiorno date unique,
+);
+create table Menu(
+	id int identity(1,1) primary key,
+	descrizione nvarchar(50)	
+);
+create table MenuPiatti(
+	id int identity(1,1) primary key,
+	menu int foreign key references Menu, 
+	piatto int foreign key references Piatti,
+	unique(menu,piatto)	
+);
+create table GiorniMenu(
+	id int identity(1,1) primary key,
+	menu int foreign key references Menu, 
+	giorno int foreign key references Giorni,
+	fasciaOrario int foreign key references FasciaOrario,
+	unique(menu,giorno,fasciaOrario)	
+);
+create procedure AddMenu
+	@nomeMenu nvarchar(50),
+	@data date,
+	@primo nvarchar(50),
+	@secondo nvarchar(50),
+	@contorno nvarchar(50),
+	@dolce nvarchar(50)
+as
+	set implicit_transactions on;
+	begin try
+		insert into Giorni(dataGiorno) values(@data);
+		declare @idG int = ident_current('Giorni');
+		insert into Menu(descrizione) values(@nomeMenu);
+		declare @idM int = ident_current('Menu');
+		
+	end try 
+go
---
+++ EsercizioMvc/Models/DAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EsercizioMvc.Models {
+    interface IDAO {
+        void Add(Menu menu);
+        List<Menu> GetListMenu();
+        Menu GetMenu(string nameMenu);
+    }
+    public class DAO : IDAO {
+        public GestioneMenuEntities db;
+        public void Add(Menu menu) {
+            using(db = new GestioneMenuEntities()) {
+                db.Menu
+            }
+        }
+
+        public List<Menu> GetListMenu() {
+            throw new NotImplementedException();
+        }
+
+        public Menu GetMenu(string nameMenu) {
+            throw new NotImplementedException();
+        }
+    }
+}
